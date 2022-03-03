1fa124d556efaa696fdaa70c24eee4fbcd3094b6
Syabila Ilma Elsani <Syabila Ilma Elsani@SYABILA-PC>
2022-03-03 19:14:43 +0700

    Add project files.

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_input = new System.Windows.Forms.TextBox();
+            this.label_output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text_input
+            // 
+            this.text_input.Location = new System.Drawing.Point(312, 164);
+            this.text_input.Name = "text_input";
+            this.text_input.Size = new System.Drawing.Size(180, 26);
+            this.text_input.TabIndex = 1;
+            // 
+            // label_output
+            // 
+            this.label_output.AutoSize = true;
+            this.label_output.Location = new System.Drawing.Point(308, 258);
+            this.label_output.Name = "label_output";
+            this.label_output.Size = new System.Drawing.Size(0, 20);
+            this.label_output.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 525);
+            this.Controls.Add(this.label_output);
+            this.Controls.Add(this.text_input);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text_input;
+        private System.Windows.Forms.Label label_output;
+    }
+}
+
